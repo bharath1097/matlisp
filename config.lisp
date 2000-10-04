aab557974b1f12736e75a06c5712305ec6328eeb
simsek <simsek>
2000-10-04 22:47:46 +0000

    o Cleared out most environment settings

--- config.lisp
+++ config.lisp
@@ -27,79 +27,20 @@
 ;;;
 ;;; This is the configuration file for the MATLISP workspace.
 ;;;
+;;; Matlisp is a package and should really not set any environment 
+;;; configuration. Eventually, this file should disappear.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; $Id: config.lisp,v 1.1 2000/07/10 22:38:43 simsek Exp $
+;;; $Id: config.lisp,v 1.2 2000/10/04 22:47:46 simsek Exp $
 ;;; $Log: config.lisp,v $
+;;; Revision 1.2  2000/10/04 22:47:46  simsek
+;;; o Cleared out most environment settings
+;;;
 ;;; Revision 1.1  2000/07/10 22:38:43  simsek
 ;;; o Initial revision
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(in-package "COMMON-LISP-USER")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Turn off compilation messages.  It is annoying to see 
-;;; most messages during compilation.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq *compile-print* nil)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Make I,J available as the complex identity.
-;;; Use DEFPARAMETER and not DEFVAR so that they
-;;; may be redefined.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-#|
-(unexport '(i j))
-(export '(i j))
-(defparameter i #c(0.0d0 1.0d0))
-(defparameter j #c(0.0d0 1.0d0))
-|#
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Read all floating points numbers as DOUBLE-FLOAT,
-;;; using SINGLE-FLOAT does not account for efficiency
-;;; since most machines are optimized for DOUBLE-FLOAT
-;;; operations.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq *read-default-float-format* 'double-float)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Make QUIT, EXIT, BYE available in all packages that
-;;; use the COMMON-LISP-USER package.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-#+:cmu
-(progn
-  (unexport 'quit "COMMON-LISP")
-  (export 'quit "COMMON-LISP")
-  (unexport '(exit bye))
-  (export '(exit bye))
-
-  (unless (fboundp 'bye)
-    (defun bye ()
-      (common-lisp::quit)))
-
-  (unless (fboundp 'exit)
-    (defun exit ()
-      (common-lisp::quit))))
-
-#+:allegro
-(progn
-  (unexport '(quit bye exit))
-  (export '(quit bye exit))
-  
-  (unless (fboundp 'quit)
-    (defun quit ()
-      (common-lisp-user::exit)))
-
-  (unless (fboundp 'bye)
-    (defun bye ()
-      (common-lisp-user::exit))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Turn off GC messages for CMU, it is annoying 
-;;; during compilation.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-#+:cmu (setq ext:*gc-verbose* nil) 
-#+:allegro (setq excl:*global-gc-behavior* :auto)
+(in-package "MATLISP")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Make sure that PCL compiles and evaluates DEFCLASS
@@ -110,8 +51,4 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 #+:allegro (setq comp:*cltl1-compile-file-toplevel-compatibility-p* t)
 #+:allegro (setq excl:*enable-package-locked-errors* nil)
-
-#+:cmu (pushnew 'compile pcl::*defclass-times*)
-#+:cmu (pushnew 'compile pcl::*defgeneric-times*)
-
 
