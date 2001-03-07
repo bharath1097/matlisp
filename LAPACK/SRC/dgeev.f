8aadf0aa6c35a264ac680cfd0b9d05d6bcc9b1f0
Raymond Toy <toy.raymond@gmail.com>
2001-03-07 00:16:34 +0000

    The workspace size query doesn't work for dgeev.  Code was modified to look like zgeev, where the workspace size query does work.

--- LAPACK/SRC/dgeev.f
+++ LAPACK/SRC/dgeev.f
@@ -176,7 +176,7 @@
 *       the worst case.)
 *
       MINWRK = 1
-      IF( INFO.EQ.0 .AND. LWORK.GE.1 ) THEN
+      IF( INFO.EQ.0 .AND. (LWORK.GE.1 .OR. LQUERY)) THEN
          MAXWRK = 2*N + N*ILAENV( 1, 'DGEHRD', ' ', N, 1, N, 0 )
          IF( ( .NOT.WANTVL ) .AND. ( .NOT.WANTVR ) ) THEN
             MINWRK = MAX( 1, 3*N )
