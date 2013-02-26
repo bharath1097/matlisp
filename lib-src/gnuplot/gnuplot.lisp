8d1294ce927280b8f2633b1e8636bd8aacfbf45d
Akshay Srinivasan <akshaysrinivasan@gmail.com>
2013-02-25 22:33:15 -0800

    Minor changes to gnuplot.lisp.

--- lib-src/gnuplot/gnuplot.lisp
+++ lib-src/gnuplot/gnuplot.lisp
@@ -8,24 +8,30 @@
    ccl:run-program
    gnuplot-binary nil :input :stream :wait nil :output t))
 
-(defun plot2d (data &key (color (list "#FF0000")) (stream (#+:sbcl
-							   sb-ext:process-input
-							   #+:ccl
-							   ccl:external-process-input-stream
-							   *current-gnuplot-process*)))							       
-  (with-open-file (s "/tmp/matlisp-gnuplot.out" :direction :output :if-exists :supersede :if-does-not-exist :create)
-    (loop :for i :from 0 :below (loop :for x :in data :minimizing (number-of-elements x))
-       :do (loop :for x :in data :do (format s "~a " (tensor-ref x i)) :finally (format s "~%"))))
-  (format stream "plot '/tmp/matlisp-gnuplot.out' with lines linecolor rgb ~s~%" color)
-  (finish-output stream))
+(defun plot2d (data &key (color (list "#FF0000")))
+  (unless *current-gnuplot-process*
+    (setf *current-gnuplot-process* (open-gnuplot-stream)))
+  (let ((stream (#+:sbcl
+		 sb-ext:process-input
+		 #+:ccl
+		 ccl:external-process-input-stream
+		 *current-gnuplot-process*)))
+    (with-open-file (s "/tmp/matlisp-gnuplot.out" :direction :output :if-exists :supersede :if-does-not-exist :create)
+      (loop :for i :from 0 :below (loop :for x :in data :minimizing (number-of-elements x))
+	 :do (loop :for x :in data :do (format s "~a " (tensor-ref x i)) :finally (format s "~%"))))
+    (format stream "plot '/tmp/matlisp-gnuplot.out' with lines linecolor rgb ~s~%" color)
+    (finish-output stream)))
 
-(defun gnuplot-send (str &key (stream (#+:sbcl
-				       sb-ext:process-input
-				       #+:ccl
-				       ccl:external-process-input-stream
-				       *current-gnuplot-process*)))
-  (format stream "~a~%" str)
-  (finish-output stream))
+(defun gnuplot-send (str)
+  (unless *current-gnuplot-process*
+    (setf *current-gnuplot-process* (open-gnuplot-stream)))
+  (let (stream (#+:sbcl
+		sb-ext:process-input
+		 #+:ccl
+		 ccl:external-process-input-stream
+		 *current-gnuplot-process*))
+    (format stream "~a~%" str)
+    (finish-output stream)))
 
 
 ;; (defclass gnuplot-plot-info ()
