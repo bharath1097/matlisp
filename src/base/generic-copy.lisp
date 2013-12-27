6dacaaaa8356ad476ac631eb95b93829a5f1e3f1
Akshay Srinivasan <akshaysrinivasan@gmail.com>
2013-12-27 17:08:57 +0530

    Added optional type for "copy" generic function.

--- src/base/generic-copy.lisp
+++ src/base/generic-copy.lisp
@@ -12,14 +12,14 @@
   Copies the contents of X into Y. Returns Y.
 ")
   (:method :before ((x array) (y array))
-	   (assert (list-eq (array-dimensions x) (array-dimensions y))		   
+	   (assert (list-eq (array-dimensions x) (array-dimensions y))
 		   nil 'dimension-mismatch)))
 
 (defmethod copy! ((from cons) (to cons))
-  (let-rec cdr-writer ((flst from) (tlst to))
-    (unless (or (null flst) (null tlst))
-      (setf (car tlst) (car flst))
-      (cdr-writer (cdr flst) (cdr tlst))))
+  (do ((flst from (cdr flst))
+       (tlst to (cdr tlst)))
+      ((or (null flst) (null tlst)))
+    (setf (car tlst) (car flst)))
   to)
 
 (defmethod copy! ((from t) (to cons))
@@ -57,10 +57,10 @@
      `(defmethod copy! ((x array) (y ,clname))
 	(let-typed ((sto-y (store y) :type (simple-array ,(store-element-type clname)))
 		    (lst (make-list (array-rank x)) :type cons))
-		   (mod-dotimes (idx (dimensions y))
-		     :with (linear-sums
-			    (of-y (strides y) (head y)))
-		     :do (t/store-set ,clname (t/coerce ,(field-type clname) (apply #'aref x (lvec->list! idx lst))) sto-y of-y)))
+	  (mod-dotimes (idx (dimensions y))
+	    :with (linear-sums
+		   (of-y (strides y) (head y)))
+	    :do (t/store-set ,clname (t/coerce ,(field-type clname) (apply #'aref x (lvec->list! idx lst))) sto-y of-y)))
 	y))
     (copy! x y)))
 
@@ -71,10 +71,10 @@
      `(defmethod copy! ((x ,clname) (y array))
 	(let-typed ((sto-x (store x) :type (simple-array ,(store-element-type clname)))
 		    (lst (make-list (array-rank y)) :type cons))
-		   (mod-dotimes (idx (dimensions x))
-		     :with (linear-sums
-			    (of-x (strides x) (head x)))
-		     :do (setf (apply #'aref y (lvec->list! idx lst)) (t/store-ref ,clname sto-x of-x))))
+	  (mod-dotimes (idx (dimensions x))
+	    :with (linear-sums
+		   (of-x (strides x) (head x)))
+	    :do (setf (apply #'aref y (lvec->list! idx lst)) (t/store-ref ,clname sto-x of-x))))
 	y))
     (copy! x y)))
 
@@ -83,23 +83,47 @@
   (let ((arr (make-array (list-dimensions x) :initial-contents x)))
     (copy! arr y)))
 ;;
-(defgeneric copy (object)
-  (:documentation 
+(defgeneric copy-generic (object type)
+  (:documentation
    "
   Syntax
   ======
-  (COPY x)
- 
+  (COPY-GENERIC x type)
+
   Purpose
   =======
-  Return a copy of X"))
+  Return a copy of X coerced to TYPE"))
 
-(defmethod copy ((num number))
-  num)
+(definline copy (obj &optional type)
+  (copy-generic obj type))
 
-(defmethod copy ((lst cons))
-  (copy-list lst))
+(defmethod copy-generic ((num number) type)
+  (if type (coerce num type) num))
 
-(defmethod copy ((arr array))
-  (let ((ret (make-array (array-dimensions arr) :element-type (array-element-type arr))))
-    (copy! arr ret)))
+(defmethod copy-generic ((lst cons) type)
+  (cond
+    ((member type '(list cons nil)) (copy-tree lst))
+    ((eql type 'vector) (make-array (length lst) :initial-contents lst))
+    ((eql type 'array)
+     (make-array (list-dimensions lst) :initial-contents lst))
+    ((subtypep type 'standard-tensor)
+     (let ((ret (zeros (list-dimensions lst) type)))
+       (copy! lst ret)))
+    (t (error "don't know how to copy a list to type ~a" type))))
+
+(defmethod copy-generic ((arr array) type)
+  (cond
+    ((member type '(array nil))
+     (let ((ret (make-array (array-dimensions arr) :element-type (array-element-type arr))))
+       (copy! arr ret)))
+    ((member type '(list cons))
+     (labels ((mtree (arr idx)
+		(let ((n (length idx)))
+		  (if (= n (array-rank arr)) (apply #'aref arr idx)
+		      (loop :for i :from 0 :below (array-dimension arr n)
+			 :collect (mtree arr (append idx (list i))))))))
+       (mtree arr nil)))
+    ((subtypep type 'standard-tensor)
+     (let ((ret (zeros (array-dimensions arr) type)))
+       (copy! arr ret)))
+    (t (error "don't know how to copy a list to type ~a" type))))
