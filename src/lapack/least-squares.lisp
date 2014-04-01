72f1265aef1909ad6ae4ba8221a1b13374fd0036
Akshay Srinivasan <akshaysrinivasan@gmail.com>
2014-04-01 13:30:44 -0700

    least-squares: Moved all the workspace enquiry code into one neat template.

--- src/lapack/least-squares.lisp
+++ src/lapack/least-squares.lisp
@@ -9,50 +9,42 @@
 (deft/method t/lapack-gelsy-func (sym complex-tensor) ()
   'mzgelsy)
 ;;
-(deft/generic (t/lapack-gelsy! #'subtypep) sym (A lda B ldb rcond work))
-(deft/method t/lapack-gelsy! (sym blas-numeric-tensor) (A lda B ldb rcond work)
-  (using-gensyms (decl (A lda B ldb rcond work))
+(deft/generic (t/lapack-gelsy! #'subtypep) sym (A lda B ldb rcond))
+(deft/method t/lapack-gelsy! (sym blas-numeric-tensor) (A lda B ldb rcond)
+  (using-gensyms (decl (A lda B ldb rcond) (lwork xxx))
     (with-gensyms (jpvt)
     `(let* (,@decl
-	    (,jpvt (make-array (ncols ,A) :element-type '(unsigned-byte 32) :initial-element 0)))
+	    (,jpvt (make-array (ncols ,A) :element-type '(unsigned-byte 32) :initial-element 0))
+	    (,lwork -1))
        (declare (type ,sym ,A ,B)
-		(type index-type ,lda ,ldb)
+		(type index-type ,lda ,ldb ,lwork)
 		;;BEWARE: This will throw an error, if you use (simple-array (complex double-float) (*)) for store.
 		(type ,(store-element-type sym) ,rcond)
-		(type ,(store-type sym) work)
 		(type (simple-array (unsigned-byte 32) (*)) ,jpvt))
+       (let-typed ((,xxx (t/store-allocator ,sym 1) :type ,(store-type sym)))
+	 (,(macroexpand-1 `(t/lapack-gelsy-func ,sym))
+	   (nrows ,A) (ncols ,A) (ncols ,B)
+	   ,xxx ,lda
+	   ,xxx ,ldb
+	   ,jpvt ,rcond 0
+	   ,xxx -1
+	   0)
+	 (setq ,lwork (ceiling (t/frealpart ,(field-type sym) (t/store-ref ,sym ,xxx 0)))))
        (,(macroexpand-1 `(t/lapack-gelsy-func ,sym))
 	 (nrows ,A) (ncols ,A) (ncols ,B)
 	 (the ,(store-type sym) (store ,A)) ,lda
 	 (the ,(store-type sym) (store ,B)) ,ldb
 	 ,jpvt ,rcond 0
-	 ,work (t/store-size ,sym ,work)
+	 (t/store-allocator ,sym ,lwork) ,lwork
 	 0
 	 (the index-type (head ,A)) (the index-type (head ,B)))))))
-
-(deft/generic (t/lapack-gelsy-workspace-inquiry #'subtypep) sym (m n nrhs))
-(deft/method t/lapack-gelsy-workspace-inquiry (sym blas-numeric-tensor) (m n nrhs)
-  (using-gensyms (decl (m n nrhs))
-    (with-gensyms (xxx)
-    `(let* (,@decl
-	    (,xxx (t/store-allocator ,sym 1)))
-       (declare (type index-type ,m ,n ,nrhs)
-		(type ,(store-type sym) ,xxx))
-       (,(macroexpand-1 `(t/lapack-gelsy-func ,sym))
-	 ,m ,n ,nrhs
-	 ,xxx ,m
-	 ,xxx ,m
-	 ,xxx (t/coerce (t/store-element-type ,sym) 0) 0
-	 ,xxx -1
-	 0)
-       (ceiling (t/frealpart ,(field-type sym) (t/store-ref ,sym ,xxx 0)))))))
 ;;
 (defgeneric gelsy! (A B &optional rcond)
   (:documentation "
    Syntax
    =======
 
-   (GELSY! A B &optional TOL)
+   (GELSY A B &optional TOL)
 
    INPUT
    -----
@@ -67,35 +59,35 @@
 
    Purpose
    =======
- 
+
    Compute the minimum-norm solution to a real linear least
    squares problem:
        minimize || A * X - B ||
    using a complete orthogonal factorization of A.  A is an M-by-N
    matrix which may be rank-deficient.
- 
+
    Several right hand side vectors b and solution vectors x can be
    handled in a single call; they are stored as the columns of the
    M-by-NRHS right hand side matrix B and the N-by-NRHS solution
    matrix X.
- 
+
    The routine first computes a QR factorization with column pivoting:
        A * P = Q * [ R11 R12 ]
-                   [  0  R22 ]
+		   [  0  R22 ]
    with R11 defined as the largest leading submatrix whose estimated
    condition number is less than 1/RCOND.  The order of R11, RANK,
    is the effective rank of A.
- 
+
    Then, R22 is considered to be negligible, and R12 is annihilated
    by orthogonal transformations from the right, arriving at the
    complete orthogonal factorization:
       A * P = Q * [ T11 0 ] * Z
-                  [  0  0 ]
+		  [  0  0 ]
    The minimum-norm solution is then
       X = P * Z' [ inv(T11)*Q1'*B ]
-                 [        0       ]
+		 [        0       ]
    where Q1 consists of the first RANK columns of Q.
- 
+
    This routine is basically identical to the original xGELSX except
    three differences:
      o The call to the subroutine xGEQPF has been substituted by the
@@ -104,15 +96,15 @@
      o Matrix B (the right hand side) is updated with Blas-3.
      o The permutation of matrix B (the right hand side) is faster and
        more simple.
- 
+
    Further Details
    ===============
- 
+
    Based on contributions by
      A. Petitet, Computer Science Dept., Univ. of Tenn., Knoxville, USA
      E. Quintana-Orti, Depto. de Informatica, Universidad Jaime I, Spain
      G. Quintana-Orti, Depto. de Informatica, Universidad Jaime I, Spain
- 
+
    =====================================================================
 ")
   (:method :before ((A standard-tensor) (B standard-tensor) &optional rcond)
@@ -120,17 +112,18 @@
 	   (assert (or (null rcond) (> rcond 0)) nil 'invalid-value :expected '(> rcond 0) :given rcond :message "Invalid rcond.")))
 
 (define-tensor-method gelsy ((A standard-tensor :input) (B standard-tensor :input) &optional (rcond *default-rcond*))
-  `(let* ((A (with-colm (copy A ',(cl b))))
-	  (lwork (max (t/lapack-gelsy-workspace-inquiry ,(cl b) (nrows A) (ncols A) (ncols B)) 1))
-	  (work (t/store-allocator ,(cl b) lwork)))
-     (declare (type index-type lwork)
-	      (type ,(store-type (cl b)) work)
-	      (type ,(cl b) A))
+  `(let* ((A (with-colm (copy A ',(cl b)))))
+     (declare (type ,(cl b) A))
      (let* ((mn (max (nrows A) (ncols A)))
 	    (X (with-colm (zeros (list mn (ncols B)) ',(cl b)))))
        (copy! B (subtensor~ X `((0 ,(nrows B)) (nil nil)) t))
-       (multiple-value-bind (sto-a sto-b jpvt rank work-out info) (t/lapack-gelsy! ,(cl b) A (or (blas-matrix-compatiblep A #\N) 0) X (or (blas-matrix-compatiblep X #\N) 0) rcond work)
+       (multiple-value-bind (sto-a sto-b jpvt rank work-out info) (t/lapack-gelsy! ,(cl b) A (or (blas-matrix-compatiblep A #\N) 0) X (or (blas-matrix-compatiblep X #\N) 0) rcond)
 	 (declare (ignore sto-a sto-b work-out jpvt))
 	 (unless (= info 0)
 	   (error "gelsy returned ~a." info))
 	 (values (copy (subtensor~ X `((0 ,(ncols A)) (nil nil)) t)) rank)))))
+
+(definline lstsq (A B &optional (rcond nil rcond-p))
+  (if rcond-p
+      (gelsy A B rcond)
+      (gelsy A B)))
