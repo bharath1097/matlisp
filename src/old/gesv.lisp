6a10ff471532a380ceaa5ebd9ddce1afe9985810
Akshay Srinivasan <akshaysrinivasan@gmail.com>
2014-05-24 20:18:26 -0700

    will not be moving gesv.

--- src/old/gesv.lisp
+++ src/old/gesv.lisp
@@ -25,49 +25,7 @@
 ;;; ENHANCEMENTS, OR MODIFICATIONS.
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Originally written by Raymond Toy
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; $Id: gesv.lisp,v 1.4 2000/07/11 18:02:03 simsek Exp $
-;;;
-;;; $Log: gesv.lisp,v $
-;;; Revision 1.4  2000/07/11 18:02:03  simsek
-;;; o Added credits
-;;;
-;;; Revision 1.3  2000/07/11 02:11:56  simsek
-;;; o Added support for Allegro CL
-;;;
-;;; Revision 1.2  2000/05/08 17:19:18  rtoy
-;;; Changes to the STANDARD-MATRIX class:
-;;; o The slots N, M, and NXM have changed names.
-;;; o The accessors of these slots have changed:
-;;;      NROWS, NCOLS, NUMBER-OF-ELEMENTS
-;;;   The old names aren't available anymore.
-;;; o The initargs of these slots have changed:
-;;;      :nrows, :ncols, :nels
-;;;
-;;; Revision 1.1  2000/04/14 00:11:12  simsek
-;;; o This file is adapted from obsolete files 'matrix-float.lisp'
-;;;   'matrix-complex.lisp' and 'matrix-extra.lisp'
-;;; o Initial revision.
-;;;
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(in-package "MATLISP")
-
-#+:nil (use-package "BLAS")
-#+:nil (use-package "LAPACK")
-#+:nil (use-package "FORTRAN-FFI-ACCESSORS")
-
-#+:nil (export '(gesv!
-		 gesv))
-
-
-#+:pre-allocate-workspaces
-(defvar *ipiv* (make-array *ipiv-size* :element-type '(unsigned-byte 32))) 
+(in-package #:matlisp)
 
 (defgeneric gesv! (a  b &key ipiv)
   (:documentation
