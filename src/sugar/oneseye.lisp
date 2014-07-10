079ea856dba23691bde5bb3549258bb038735658
Akshay Srinivasan <akshaysrinivasan@gmail.com>
2014-07-10 15:42:41 -0700

    removed restart-case from potrf!, added neg-axis support to sum, added diag~

--- src/sugar/oneseye.lisp
+++ src/sugar/oneseye.lisp
@@ -12,3 +12,14 @@
 (defun diag (tens &optional (order 2))
   (let ((ret (zeros (make-list order :initial-element (dimensions tens 0)) (type-of tens))))
     (tricopy! tens ret :d)))
+
+(defun diag~ (a)
+  (declare (type standard-tensor a))
+  (let ((str (lvec-foldr #'(lambda (x y) (declare (type index-type x y)) (the index-type (+ x y))) (strides a)))
+	(dim (lvec-min (dimensions a))))
+    (with-no-init-checks (make-instance (class-of a)
+					:parent-tensor a
+					:store (store a)
+					:dimensions (make-index-store (list dim))
+					:head (head a)
+					:strides (make-index-store (list str))))))
