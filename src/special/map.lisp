c081e8620ceb0791649db69bff4996bbb2d0b1aa
Akshay Srinivasan <akshaysrinivasan@gmail.com>
2014-07-15 18:28:57 -0700

    added map-tensor! macro, removed old commented macros.

--- src/special/map.lisp
+++ src/special/map.lisp
@@ -1,5 +1,6 @@
 (in-package #:matlisp)
 
+;;
 (defgeneric mapsor! (func x y)
   (:documentation
 "
@@ -37,13 +38,10 @@
 	    nil 'tensor-abstract-class :tensor-class (list clx cly))
     (compile-and-eval
      `(defmethod mapsor! ((func function) (x ,clx) (y ,cly))
-	(let-typed ((sto-x (store x) :type ,(store-type clx))
-		    (sto-y (store y) :type ,(store-type cly)))
-	  (mod-dotimes (idx (dimensions x))
-	    :with (linear-sums
-		   (of-x (strides x) (head x))
-		   (of-y (strides y) (head y)))
-	    :do (t/store-set ,cly (funcall func (lvec->list idx) (t/store-ref ,clx sto-x of-x) (t/store-ref ,cly sto-y of-y)) sto-y of-y)))
+	(dorefs (idx (dimensions x))
+		((ref-x x :type ,clx)
+		 (ref-y y :type ,cly))
+		(setf ref-y (funcall func (lvec->list idx) ref-x ref-y)))
 	y)))
   (mapsor! func x y))
 
@@ -51,24 +49,14 @@
   (let ((ret (zeros (dimensions x) (or output-type (class-of x)))))
     (mapsor! #'(lambda (idx x y) (declare (ignore idx y)) (funcall func x)) x ret)))
 
-;;
-;; (defmacro fapply ((ref tensa &key ttype) &optional (loop-optimizations '(progn)) &rest body)
-;;   (with-gensyms (tens sto idx of)
-;;     `(let ((,tens ,tensa))
-;;        (declare (type ,ttype ,tens))
-;;        (let ((,sto (store ,tens)))
-;;	 (declare (type ,(store-type ttype) ,sto))
-;;	 (,@loop-optimizations
-;;	   (mod-dotimes (,idx (dimensions ,tens))
-;;	     :with (linear-sums
-;;		    (,of (strides ,tens) (head ,tens)))
-;;	     :do (let ((,ref (t/store-ref ,ttype ,sto ,of)))
-;;		   (declare (type ,(field-type ttype) ,ref))
-;;		   (t/store-set ,ttype
-;;				(progn
-;;				  ,@body)
-;;				,sto ,of))))
-;;	 ,tens))))
+(defmacro map-tensor! (type x func)
+  (using-gensyms (decl (x) (idx ref))
+    `(let (,@decl)
+       (declare (type ,type ,x))
+       (very-quickly (dorefs (,idx (dimensions ,x))
+			     ((,ref ,x :type ,type))
+			     (setf ,ref (,func ,ref))))
+       ,x)))
 
 ;;
 (defun check-dims (axlst tensors)
@@ -171,64 +159,6 @@
   (if-let (s (gethash 'slice-increment (attributes x)))
     (progn (incf (slot-value x 'head) (* step s)) x)
     (error 'tensor-error :message "Can't find slice-increment in tensor's attributes" :tensor x)))
-
-#+nil(defmacro-clause (FOR xa SLICING x ALONG axis &optional FROM start BELOW oend TO cend DOWNTO dend)
-  (let* ((xa (if (listp xa) xa (list xa))))
-    (when (or (and oend cend) (and dend (or cend oend))) (error "Use only one of BELOW TO DOWNTO."))
-    (unless (null xa)
-      (with-gensyms (dim sa ssym alist asym xlist xsym slst tmp-axis tmp-dim tmp-x x-eval)
-	(let* ((n (length xa)))
-	  `(progn
-	     (with ,x-eval = ,x)
-	     (with ,dim = -1)
-	     (with ,sa = nil)
-	     ,@(mapcan #'(lambda (x s) `((with ,x = nil))) xa)
-	     (initially
-	      (let* ((,alist ,(with-gensyms (axis-eval)
-			       `(let ((,axis-eval ,axis))
-				  (if (listp ,axis-eval)
-				      (assert (= (length ,axis-eval) ,n) nil 'invalid-arguments)
-				      (setq ,axis-eval (make-list ,n :initial-element ,axis-eval)))
-				  ,axis-eval)))
-		     (,xlist (let ((,x-eval (if (listp ,x-eval)
-						(progn (assert (= (length ,x-eval) ,n) nil 'invalid-arguments) ,x-eval)
-						(make-list ,n :initial-element ,x-eval))))
-			       (iter (for ,xsym in ,x-eval)
-				     (for ,asym on ,alist)
-				     (etypecase ,xsym
-				       (null (collect nil))
-				       (standard-tensor
-					(let* ((,tmp-axis (modproj (car,asym) (order ,xsym))))
-					  (let ((,tmp-x ,(if (or start oend cend dend)
-							     `(let ((,tmp-dim (dimensions ,xsym ,tmp-axis))
-								    (,slst (make-list (order ,xsym) :initial-element '(nil nil))))
-								(declare (ignorable ,tmp-dim))
-								(setf (nth ,tmp-axis ,slst) ,(cond
-											      (dend `(list* ,start (- (modproj ,dend ,tmp-dim) ,tmp-dim 1) -1))
-											      (oend `(list ,start ,oend))
-											      (cend `(list ,start (1+ (modproj ,cend ,tmp-dim))))
-											      (t `(list ,start nil))))
-								(subtensor~ ,xsym ,slst))
-							     xsym)))
-					    (when (or (< ,dim 0) (> ,dim (dimensions ,tmp-x ,tmp-axis)))
-					      (setf ,dim (dimensions ,tmp-x ,tmp-axis)))
-					    (setf (car ,asym) ,tmp-axis)
-					    (collect ,tmp-x)))))))))
-		(setf ,@(mapcan #'(lambda (x s) `((values ,x ,s) (let* ((,tmp-axis (car ,alist))
-									(,tmp-x (car ,xlist))
-									(,ssym (if ,tmp-x (strides ,tmp-x ,tmp-axis) 0))
-									(,xsym (when ,tmp-x
-										 ,(with-gensyms (tmp)
-										    `(let ((,tmp (slice~ ,tmp-x ,tmp-axis)))
-										       (setf (gethash 'slice-increment (attributes ,tmp)) ,ssym)
-										       ,tmp)))))
-								   (values ,xsym ,ssym))
-						  ,xlist (cdr ,xlist)
-						  ,alist (cdr ,alist)))
-				xa sa))))
-	     (repeat ,dim)
-	     (after-each
-	       ,@(mapcar #'(lambda (x s) `(when ,x (incf (slot-value ,x 'head) ,s))) xa sa))))))))
 
 (defmacro-clause (FOR xa SLICING x ALONG axis &optional FROM start BELOW oend TO cend DOWNTO dend)
   (let* ((xa (if (listp xa) xa (list xa))))
